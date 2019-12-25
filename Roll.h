823f495bfc11e17d546f2a93ddad6063cbdc507f
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-25 15:46:27 -0800

    Create Roll.h

---
+++ Roll.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+class Roll
+{
+  public:
+    //void Roll();
+    void checkResult();
+    void getSSR();
+    void getSR();
+    void getR();
+    void get5CE();
+    void get4CE();
+    void get3CE();
+    void roll();
+  private:
+    //int roll;
+};
